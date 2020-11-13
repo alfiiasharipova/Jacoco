3580c9ebfb36348b5b212c2a6c4f46b5f6130304
alfiiasharipova <alfiiasharipova@gmail.com>
2020-11-14 00:56:12 +0300

    Build failed

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -9,6 +9,7 @@ public class StatisticsService {
    */
   public long findMax(long[] incomes) {
     long current_max_index = 0;
+    Number[] arr = new Integer[10];
     long current_max = incomes[0];
     int count = 0;
     for (long income : incomes) {
