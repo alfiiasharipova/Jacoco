b2ec03448a09761e83c60360bf3641acd09ceb8b
alfiiasharipova <alfiiasharipova@gmail.com>
2020-11-14 00:22:47 +0300

    Coverage 100

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,8 @@ class StatisticsServiceTest {
   void findMaxWithoutIf() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 0;
+    long[] incomesInBillions = {1, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 1;
 
     long actual = service.findMax(incomesInBillions);
 
